f40fdf58c510b9e87b7ad3b2fc0508c389446ac4
VITOR EDUARDO CONCESSO DIAS PB003402 <pb003402@pbh.rmi>
2019-03-14 16:34:01 -0300

    endpoints

--- src/main/java/com/gestaobhmap/controller/UserApplicationController.java
+++ src/main/java/com/gestaobhmap/controller/UserApplicationController.java
@@ -1,9 +1,15 @@
 package com.gestaobhmap.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.jboss.jandex.TypeTarget.Usage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestaobhmap.model.UserApplication;
@@ -18,7 +24,7 @@ public class UserApplicationController {
 	@GetMapping("/applications")
 	public List<UserApplication> teste() {
 		
-		for(int i = 0;i <10;i++) {
+		for(int i = 1;i <=2;i++) {
 			UserApplication user = new UserApplication("App"+ i);
 			userApplicationRepository.save(user);
 		}
@@ -27,4 +33,26 @@ public class UserApplicationController {
 		return userApplicationRepository.findAll();
 	}
 	
+	@GetMapping("/applications/{id}")
+	public Optional<UserApplication> buscaApplication(@PathVariable Long id) {
+		
+		
+		return userApplicationRepository.findById(id);
+	}
+	
+	@PutMapping("/applications/{id}")
+	public ResponseEntity<Object> updateStudent(@RequestBody UserApplication userApplication, @PathVariable long id) {
+
+		/*
+		 * Optional<UserApplication> studentOptional =
+		 * userApplicationRepository.findById(id);
+		 * 
+		 * if (!studentOptional.isPresent()) return ResponseEntity.notFound().build();
+		 */
+		userApplication.setId(id);
+		
+		userApplicationRepository.save(userApplication);
+
+		return ResponseEntity.noContent().build();
+	}
 }
